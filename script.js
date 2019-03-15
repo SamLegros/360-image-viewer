482f6a64061d0bfc9db126a6c9bb8054f3923113
Sam Legros <sam.legros@juicemobile.com>
2019-03-15 15:42:40 -0400

    Add march 2019 images.
    Make page sizing responsive.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // define variables ===========================================================================
 // html
 let contentDiv = document.getElementById('content-div');
+let panoramaContainerDiv = document.getElementById('panorama-container-div');
 let panoramaDiv = document.getElementById('panorama');
+let panoramaWidthRatio = 2;
+let panoramaHeightRatio = 1;
+let grid201903Container = document.getElementById('grid-2019-03-container').firstElementChild;
 let grid201902Container = document.getElementById('grid-2019-02-container').firstElementChild;
 let grid201901Container = document.getElementById('grid-2019-01-container').firstElementChild;
 
@@ -9,21 +13,39 @@ let grid201901Container = document.getElementById('grid-2019-01-container').firs
 let image = {'2019': {}};
 image['2019']['01_january'] = 1;
 image['2019']['02_february'] = 14;
+image['2019']['03_march'] = 12;
 
 
 // define event listeners =====================================================================
+for (let i = 0; i < grid201903Container.children.length; i++) {
+	grid201903Container.children[i].addEventListener('click', handleClick);
+}
+
 for (let i = 0; i < grid201902Container.children.length; i++) {
 	grid201902Container.children[i].addEventListener('click', handleClick);
 }
+
 for (let i = 0; i < grid201901Container.children.length; i++) {
 	grid201901Container.children[i].addEventListener('click', handleClick);
 }
+
 window.addEventListener('resize', handleResize);
 
 
 // define event listener functions ============================================================
 function handleResize() {
-	// console.log('resize');
+	let cssBodyMaxWidth = 1000;
+	let cssBodyPadding = 20;
+	let newWidth;
+	if (window.innerWidth <= cssBodyMaxWidth) {
+		newWidth = (window.innerWidth-(cssBodyPadding*2));
+	} else {
+		newWidth = (cssBodyMaxWidth-(cssBodyPadding*2));
+	}
+	newHeight = (panoramaHeightRatio / panoramaWidthRatio) * (newWidth);
+
+	panoramaDiv.style.width = `${newWidth}px`;
+	panoramaDiv.style.height = `${newHeight}px`;
 }
 
 function handleClick() {
@@ -40,12 +62,17 @@ function handleClick() {
 
 // define functions ===========================================================================
 function init() {
-	console.log(`Take 5`);
-	grid201902Container.children[13].click();
+	console.log(`Take 1`);
+	handleResize();
+	grid201903Container.children[11].click();
 }
 
 function updateThumbnail(data) {
 	let idInt = parseInt(data.image)-1;
+
+	for (let i = 0; i < grid201903Container.children.length; i++) {
+		grid201903Container.children[i].classList.remove('thumbnail-active');
+	}
 
 	for (let i = 0; i < grid201902Container.children.length; i++) {
 		grid201902Container.children[i].classList.remove('thumbnail-active');
@@ -59,6 +86,8 @@ function updateThumbnail(data) {
 		grid201901Container.children[idInt].classList.add('thumbnail-active');
 	} else if (data.month == '02') {
 		grid201902Container.children[idInt].classList.add('thumbnail-active');
+	} else if (data.month == '03') {
+		grid201903Container.children[idInt].classList.add('thumbnail-active');
 	}
 }
 
